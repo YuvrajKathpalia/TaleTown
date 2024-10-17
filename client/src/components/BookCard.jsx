4e23886f2e57cb26dd99c754c97672ec4c1140f9
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2024-10-18 01:40:18 +0530

    minor modification

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const BookCard = ({ book }) => {
 
@@ -11,7 +11,7 @@ const BookCard = ({ book }) => {
   };
   return (
     <div
-      className="relative w-[16rem] h-[21rem] flex flex-col items-center overflow-hidden bg-white border border-gray-300 shadow-md rounded-lg transition-transform duration-300 ease-in-out cursor-pointer hover:scale-105"
+      className="relative w-[16rem] h-[22rem] flex flex-col items-center overflow-hidden bg-white border border-gray-300 shadow-md rounded-lg transition-transform duration-300 ease-in-out cursor-pointer hover:scale-105"
       onClick={handleClick}
       style={{ boxShadow: '0 5px 15px rgba(0, 0, 0, 0.2)', transition: 'box-shadow 0.3s ease' }}
       onMouseEnter={(e) => (e.currentTarget.style.boxShadow = '0 20px 40px rgba(0, 0, 0, 0.4)')}
@@ -26,7 +26,7 @@ const BookCard = ({ book }) => {
       </div>
   
       <div className="p-4 flex flex-col h-1/3">
-        <h3 className="text-lg font-semibold mb-1 truncate transition-colors duration-300 ease-in-out hover:text-purple-500">{book.title}</h3>
+        <h3 className="text-2xl font-afacadFlux font-semibold mb-1 truncate transition-colors duration-300 ease-in-out hover:text-purple-500">{book.title}</h3>
         <p className="text-gray-700 italic mb-1 truncate transition-colors duration-300 ease-in-out hover:text-purple-400">By {book.author}</p>
         <p className="text-gray-600 font-medium transition-colors duration-300 ease-in-out hover:text-gray-800">{book.price ? `$${book.price}` : 'Price not available'}</p>
       </div>
